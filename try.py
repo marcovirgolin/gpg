1e0ab6727c471ac89cd8917716fe8376906891e6
marcovirgolin <virgolin@meteor09.scilens.private>
2022-09-27 16:50:27 +0200

    some trials with sympy to get gradients

--- try.py
+++ try.py
@@ -19,3 +19,34 @@ print(r2_score(y, p))
 
 m = g.model()
 print(m)
+
+
+import sympy
+
+def _coeffs_to_symbs(model):
+  model2 = model
+  coeff_init_values = list()
+  coeff_symbols = list()
+  symb_counter = 0
+  for node in sympy.preorder_traversal(model):
+      if isinstance(node, sympy.Float):
+        symb = "theta_"+str(symb_counter)
+        model2 = model2.subs(node, symb)
+        coeff_init_values.append(node)
+        coeff_symbols.append(symb)
+        symb_counter += 1
+  return model2, coeff_symbols, coeff_init_values
+
+print(_coeffs_to_symbs(m))
+
+ms, cs, ci = _coeffs_to_symbs(m)
+
+
+def _get_gradient(model, coeff_symbols):
+  for i, symb in enumerate(coeff_symbols):
+    deriv = sympy.diff(model, symb)
+    print('grad[{}]'.format(i), '=', sympy.ccode(deriv) + ';')
+
+_get_gradient(ms, cs)
+g = sympy.lambdify(cs, ms, modules="numpy")
+print(g(0.99,0.1))
